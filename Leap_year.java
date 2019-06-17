a66149c6bddec658a41a060a983c9bd489931726
Sarathkumar2925 <51906735+Sarathkumar2925@users.noreply.github.com>
2019-06-17 17:02:03 +0530

    Add files via upload

---
+++ Leap_year.java
@@ -0,0 +1,23 @@
+package Sarath;
+
+import java.util.Scanner;
+
+public class Leap_year
+{
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		{
+		if(a%4==0)
+		{
+			System.out.println("Yes");
+			
+		}		
+		else 
+		{
+			System.out.println("No");
+		}
+		}
+	}	
+}
