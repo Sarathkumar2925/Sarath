b2289a541822c63aa38c1abcc3a1135e382e9700
Sarathkumar2925 <51906735+Sarathkumar2925@users.noreply.github.com>
2019-06-17 17:07:22 +0530

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,14 @@
+package Sarath;
+
+import java.util.Scanner;
+
+public class Loop {
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	int a=s.nextInt();
+	for(int i=1;i<=a;i++)
+	{
+		System.out.println("Hello");
+	}s.close();
+}
+}
